b5275d04a203d9de57f6328cb9af2e4404660db3
Isidro Butaslac <butaslaciii@gmail.com>
2023-02-19 22:36:49 +0900

    ADD-ScriptInteraction-ToggleMenuBillboard
    added the interaction script for toggling the billboard menu

---
+++ Assets/01.Scripts/02.Interaction/ToggleMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// Toggle the Menu, spawn in front, and always face the user.
+/// </summary>
+public class ToggleMenu : MonoBehaviour
+{
+    /// <remarks>
+    /// Set this to the Menu button, in case of Meta Quest 2 in the left hand
+    /// </remarks>
+    public InputActionProperty showButton;
+
+    [SerializeField]
+    private Transform headCam; // main camera of the XR rig
+    [SerializeField]
+    private float spawnDistance = 1.5f; // spawn the menu 1.5m in front of user
+    [SerializeField]
+    private GameObject canvas;
+    
+    private void Update()
+    {
+        // toggle the menu on/off
+        if (showButton.action.WasPressedThisFrame())
+        {
+            canvas.SetActive(!canvas.activeSelf);
+
+            canvas.transform.position = headCam.position + (new Vector3(headCam.forward.x, 0, headCam.forward.z).normalized * spawnDistance);
+        }
+
+        // billboard functionality of menu to always face user
+        canvas.transform.LookAt(new Vector3(headCam.position.x, canvas.transform.position.y, headCam.position.z));
+        canvas.transform.forward *= -1;
+    }
+}
