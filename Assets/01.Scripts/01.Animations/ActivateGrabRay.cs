eab2510846d102f1fca3da6243fb60b468b14ac7
Isidro Butaslac <butaslaciii@gmail.com>
2023-02-20 08:07:46 +0900

    ADD-Script-ActiveGrabRay
    add a script to enable or disable the far ray when a near interaction is possible, to avoid double instance.

---
+++ Assets/01.Scripts/01.Animations/ActivateGrabRay.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+/// <summary>
+/// Enable/Disable the far interaction w/ Ray when it is possible to do near interaction directly.
+/// </summary>
+public class ActivateGrabRay : MonoBehaviour
+{
+    [SerializeField] private GameObject leftGrabRay;
+    [SerializeField] private GameObject rightGrabRay;
+
+    [SerializeField] private XRDirectInteractor leftDirectGrab;
+    [SerializeField] private XRDirectInteractor rightDirectGrab;
+
+    private void Update()
+    {
+        leftGrabRay.SetActive(leftDirectGrab.interactablesSelected.Count == 0);
+        rightGrabRay.SetActive(rightDirectGrab.interactablesSelected.Count == 0);
+    }
+}
