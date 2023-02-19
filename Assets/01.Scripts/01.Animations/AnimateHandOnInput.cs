dbd31e8a272ae199f32c061900bdd0552aacb87b
Isidro Butaslac <butaslaciii@gmail.com>
2023-02-19 20:51:16 +0900

    ADD-Comment-AnimateHandOnInput
    add the description of what the AnimateHandOnInput script do

--- Assets/01.Scripts/01.Animations/AnimateHandOnInput.cs
+++ Assets/01.Scripts/01.Animations/AnimateHandOnInput.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/// <summary>
+/// Reads the activate/select float value from the Input Actions map
+/// then pass that value to the hand animator trigger/grip
+/// </summary>
 public class AnimateHandOnInput : MonoBehaviour
 {
     public InputActionProperty pinchAnimationAction;
